435e949b36e490ce67f9367ddb95de7c14dc86d3
caneale320 <43382482+caneale320@users.noreply.github.com>
2020-04-18 21:53:13 -0400

    Delete greeting.py

--- greeting.py
+++
@@ -1,4 +0,0 @@
-# Caleb Neale, can4ku
-print("Hey hey, this is Caleb Neale here!")
-print("I'm really interested in how CS and programming can influence the world.")
-# I wrote my first program when I was 17 years old!
